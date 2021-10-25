880827f3e560ca7e98cce9d42c32c7dbcc542e6c
RyuTatsuki <ryutatsuki@ryuguitar.jp>
2021-10-25 22:44:14 +0900

    addItem function created

---
+++ main.js
@@ -0,0 +1,32 @@
+const itemList = document.getElementById('items');
+const form = document.getElementById('addForm');
+
+// Form submit event
+form.addEventListener('submit', addItem);
+
+// Add item
+function addItem(e) {
+  e.preventDefault();
+
+  // Get input value
+  const newItem = document.getElementById('item').value;
+
+  // Create new li element
+  const li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
+
+  // Create del button element
+  const deleteBtn = document.createElement('button');
+  // Add classes to del button
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  // Append del button to li
+  li.appendChild(deleteBtn);
+  // Append li to list
+  itemList.appendChild(li);
+}
